7cd64829b9b794d9973de3d6d6d942cbb44ec830
Sheikh Sanjid <46426568+hunterzolemon@users.noreply.github.com>
2019-03-11 04:20:22 +0600

    Update epic.py

--- epic.py
+++ epic.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route("/")
 def index():
-    return "Flask App! Visit <a href='http://127.0.0.1:81/hello/Jackson/'>http://127.0.0.1/hello/Jackson/</a> "
+    return "Flask App! Visit <a href='http://127.0.0.1:5000/hello/Jackson/'>http://127.0.0.1/hello/Jackson/</a> "
  
 @app.route("/hello/<string:name>/")
 def hello(name):
@@ -12,4 +12,4 @@ def hello(name):
         'test.html',name=name)
  
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=81)
+    app.run(host='0.0.0.0', port=5000)
